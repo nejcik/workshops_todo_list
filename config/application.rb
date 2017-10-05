47fd327eb00c3ff92a44a6346d96a0a0222cd71d
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-10-05 20:23:33 +0200

    Add new task: CR

--- config/application.rb
+++ config/application.rb
@@ -12,12 +12,12 @@ module WorkshopsTodo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.action_view.field_error_proc = Proc.new { |html_tag, _|
-      class_attr_index = html_tag.index 'class="'
+      class_attr_index = html_tag.index "class='"
 
       if class_attr_index
         html_tag.insert class_attr_index+7, 'error '
       else
-        html_tag.insert html_tag.index('>'), ' class="error"'
+        html_tag.insert html_tag.index('>'), " class='error'"
       end
     }
   end
