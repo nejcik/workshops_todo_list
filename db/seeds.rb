061dda56c3c59e73004dabbc552919dd9facb307
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-10-05 21:10:21 +0200

    Readme + seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.destroy_all
+
+Task.create!([{
+  title: "Pimp this app",
+  description: "Pimp this TODO app and make it work as perfect as possible."
+},
+{
+  title: "Sign in for workshops",
+  description: "Fill in the workshops registration form and click 'submit'!"
+},
+{
+  title: "Empty your stomach",
+  description: "Prepare your stomach for pizza during workshops"
+},
+{
+  title: "Tell the world about the workshops!"
+}
+])
+
+p "Created #{Task.count} tasks"
